7bd24634dd9a1111452e5bc524a7d6dcd83e8a25
leograssi <leocadio_grassigoncalves@hotmail.com>
2023-06-14 16:18:24 -0300

    leocadio

--- Aula 12  - DOM/js/script.js
+++ Aula 12  - DOM/js/script.js
@@ -2,25 +2,25 @@ const listaPosts = [
   {
     titulo: "Lorem ipsum dolor sit amet.",
     descricao:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis nunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper tortor, iaculis viverra diam augue eget odio. Nullam sit amet massa non nunc finibus rhoncus eu id elit. Nam ut tincidunt nisl. Duis consequat hendrerit metus id fermentum. Integer risus massa, fermentum lacinia pellentesque vel, congue nec ex. Etiam et tincidunt urna. Donec dolor justo, vehicula eu tincidunt id, eleifend eu leo. Donec eu dolor sapien. Maecenas et accumsan dolor, eget placerat massa.",
+      "JUNINHO",
     imgURL: "./img/img1.jpg",
   },
   {
     titulo: "Lorem ipsum dolor sit amet.",
     descricao:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis nunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper tortor, iaculis viverra diam augue eget odio. Nullam sit amet massa non nunc finibus rhoncus eu id elit. Nam ut tincidunt nisl. Duis consequat hendrerit metus id fermentum. Integer risus massa, fermentum lacinia pellentesque vel, congue nec ex. Etiam et tincidunt urna. Donec dolor justo, vehicula eu tincidunt id, eleifend eu leo. Donec eu dolor sapien. Maecenas et accumsan dolor, eget placerat massa.",
+      "LEVI",
     imgURL: "./img/img2.jpg",
   },
   {
     titulo: "Lorem ipsum dolor sit amet.",
     descricao:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis nunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper tortor, iaculis viverra diam augue eget odio. Nullam sit amet massa non nunc finibus rhoncus eu id elit. Nam ut tincidunt nisl. Duis consequat hendrerit metus id fermentum. Integer risus massa, fermentum lacinia pellentesque vel, congue nec ex. Etiam et tincidunt urna. Donec dolor justo, vehicula eu tincidunt id, eleifend eu leo. Donec eu dolor sapien. Maecenas et accumsan dolor, eget placerat massa.",
+      "BILY",
     imgURL: "./img/img3.jpg",
   },
   {
     titulo: "Lorem ipsum dolor sit amet.",
     descricao:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus. Aliquam tempor nisi id lacinia lobortis. Donec in justo mauris. Mauris luctus tristique est, a faucibus elit. Vivamus et dapibus mi, id facilisis nunc. Etiam feugiat, sapien sed pharetra sagittis, risus sapien semper tortor, iaculis viverra diam augue eget odio. Nullam sit amet massa non nunc finibus rhoncus eu id elit. Nam ut tincidunt nisl. Duis consequat hendrerit metus id fermentum. Integer risus massa, fermentum lacinia pellentesque vel, congue nec ex. Etiam et tincidunt urna. Donec dolor justo, vehicula eu tincidunt id, eleifend eu leo. Donec eu dolor sapien. Maecenas et accumsan dolor, eget placerat massa.",
+      "GORDINHO",
     imgURL: "./img/img4.webp",
   },
 ];
@@ -34,6 +34,7 @@ const criacaoPosts = () => {
                 <img class="card-img" src=${post.imgURL} /> 
                 <h2 class="card-titulo"> ${post.titulo} </h2>
                 <p class="card-descricao"> ${post.descricao} </p>
+                <button class="card-botao" type="button"> entre em contato</button> 
             </div>
         `;
     container.innerHTML += card;
@@ -42,4 +43,36 @@ const criacaoPosts = () => {
 
 window.onload = () => {
   criacaoPosts();
+
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    console.log(card);
+    card.addEventListener("mousemove", () => {
+      card.style.backgroundColor = "yellow";
+    });
+    card.addEventListener("mouseout", () => {
+      card.style.backgroundColor = "transparent";
+    });
+  });
 };
+
+let botaoTema = document.querySelector("#botaoTema");
+console.log(botaoTema);
+
+botaoTema.addEventListener("click", () => {
+  let body = document.querySelector("body");
+
+  if (botaoTema.checked) {
+    body.classList.add("body-dark");
+  } else {
+    body.classList.remove("body-dark");
+  }
+
+});
+
+inputTeste = document.getElementById("inputTeste");
+
+inputTeste.addEventListener("keypress", (tecla) =>{
+  alert("Tecla pressionada: " + tecla.key);
+});
