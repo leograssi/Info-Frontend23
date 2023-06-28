fb71af4af30f1a31c807dfdd7f0458c144203716
leograssi <leocadio_grassigoncalves@hotmail.com>
2023-06-28 16:40:42 -0300

    leo e kaua :D

--- DOM/js/script.js
+++ DOM/js/script.js
@@ -1,25 +1,25 @@
 const listaAnimais = [
   {
     id: 1,
-    titulo: "Lorem ipsum dolor sit amet.",
+    titulo: "N/A",
     descricao: "JUNINHO",
     imgURL: "./img/img1.jpg",
   },
   {
     id: 2,
-    titulo: "Lorem ipsum dolor sit amet.",
+    titulo: "N/A",
     descricao: "LEVI",
     imgURL: "./img/img2.jpg",
   },
   {
     id: 3,
-    titulo: "Lorem ipsum dolor sit amet.",
+    titulo: "N/A",
     descricao: "BILY",
     imgURL: "./img/img3.jpg",
   },
   {
     id: 4,
-    titulo: "Lorem ipsum dolor sit amet.",
+    titulo: "N/A",
     descricao: "GORDINHO",
     imgURL: "./img/img4.webp",
   },
@@ -28,14 +28,13 @@ const listaAnimais = [
 let container = document.querySelector(".container");
 
 const criacaoPosts = () => {
-  listaPosts.forEach((post) => {
+  listaAnimais.forEach((post) => {
     let card = `   
               <div class="card">
                   <img class="card-img" src=${post.imgURL} /> 
                   <h2 class="card-titulo"> ${post.titulo} </h2>
                   <p class="card-descricao"> ${post.descricao} </p>
-                  <a class="card-botao" idAnimal="${animal.id} <p/>
-                  <button class="card-botao" type="button"> entre em contato</button> 
+                  <a class="card-botao" idAnimal="${post.id}"> Enviar </a>
               </div>
           `;
     container.innerHTML += card;
@@ -57,11 +56,12 @@ window.onload = () => {
       card.style.backgroundColor = "transparent";
     });
 
-    card.lastElementChild.addEventListener("click", () => {
+    card.lastElementChild.addEventListener("click", (event) => {
+      console.log("entrou");
       event.preventDefault();
       sessionStorage.setItem(
         "idAnimal",
-        card.lastElementChild.getAtribute("idAnimal")
+        card.lastElementChild.getAttribute("idAnimal")
       );
       window.location.href = "./adotar.html";
     });
